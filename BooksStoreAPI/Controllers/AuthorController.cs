9c9438cd4dab16f7594c9fdbe54d7669ad6c0a59
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2021-08-04 14:42:19 +0300

    db first models creation

---
+++ BooksStoreAPI/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BooksStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Author>Get()
+        {
+            using (var context = new BookStoresDBContext())
+            {
+                //get all authors
+                return context.Author.ToList();
+            }
+        }
+    }
+}
